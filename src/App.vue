4571531886cb619f22613c8ef547dd38e039dbae
Amarildo <amarildos@outloo.it>
2024-02-01 16:48:51 +0100

    added base header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <script>
-
+import AppHeader from './components/Appheader.vue';
 export default {
-
+    name: 'MyApp',
+    data: () => ({
+    }),
+    components: {
+        AppHeader,
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="">
+        <AppHeader />
 
-<style></style>
+    </div>
+</template>
+
+<style>
+/* Generics */
+* {
+    margin: 0;
+    padding: 0;
+}
+
+/* Utils */
+.container {
+    margin: 0 auto;
+    width: 800px;
+    border: 2px solid black;
+}
+
+/* Utils Flex */
+.d-flex {
+    display: flex;
+}
+
+.flex-between {
+    justify-content: space-between;
+}
+
+.align-center {
+    align-items: center;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data: () => ({
+    }),
+}
+</script>
+<template>
+    <header>
+        <div class="container d-flex flex-between align-center">
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div>
+                <ul class="d-flex">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Characters</a></li>
+                </ul>
+            </div>
+
+
+        </div>
+    </header>
+</template>
+
+<style>
+header {
+    background-color: aqua;
+}
+
+ul {
+    list-style: none;
+}
+
+li a {
+    padding: 5px;
+    text-decoration: none;
+}
+</style>
